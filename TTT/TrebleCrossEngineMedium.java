a2d7cd14e488e04f66e718741bccf09ddb526d7b
Fang <fangzhili25@gmail.com>
2023-05-25 23:41:34 +0800

    Updated strategy

--- TTT/TrebleCrossEngineMedium.java
+++ TTT/TrebleCrossEngineMedium.java
@@ -1,27 +1,37 @@
+import java.util.Random;
+
 public class TrebleCrossEngineMedium implements Engine{
-    @Override
+
+    Random random = new Random();
+
     public int getMove(char[] board, String currentPlayer) {
-        // Check for winning move
+        // Check for a winning opportunity
         for (int i = 0; i < 9-2; i++) {
-            if (board[i] == 'X' && board[i + 1] == 'X' && board[i + 2] == '-') {
+            if ((board[i] == 'X' && board[i + 1] == 'X' && board[i + 2] == '-') ||
+                    (board[i] == 'X' && board[i + 1] == '-' && board[i + 2] == 'X') ||
+                    (board[i] == '-' && board[i + 1] == 'X' && board[i + 2] == 'X')) {
                 return i + 2;
             }
         }
 
-        // Check for blocking player's winning move
+        // If no immediate win, try to avoid creating two 'X's in a sequence, unless there is no other option
+        boolean foundMove = false;
+        int move = -1;
         for (int i = 0; i < 9-2; i++) {
-            if (board[i] != '-' && board[i] == board[i + 1] && board[i + 2] == '-') {
-                return i + 2;
+            if (board[i] == '-' && board[i + 1] == '-' && board[i + 2] == '-') {
+                move = i;
+                foundMove = true;
+                break;
             }
         }
 
-        // If there's no winning or blocking move, make a random move
-        for (int i = 0; i < 9; i++) {
-            if (board[i] == '-') {
-                return i;
-            }
+        // If no safe move was found, make a random move
+        if (!foundMove) {
+            do {
+                move = random.nextInt(9);
+            } while (board[move] != '-');
         }
 
-        throw new IllegalStateException("No valid moves left.");
+        return move;
     }
 }
