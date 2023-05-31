4fbd3feab8de1cc4deba20adffcb164671c247c2
Calvin993 <calvinjmy993@gmail.com>
2023-05-31 10:44:11 +0800

    update reverse TTT engines

--- TTT/GameEngine/ReverseEngine.java
+++ TTT/GameEngine/ReverseEngine.java
@@ -1,113 +1,106 @@
 package GameEngine;
 
 import java.util.Scanner;
-import java.util.Random;
 
-class ReverseEngine extends Engine {
-    Random random = new Random();
-    private String winner = "draw";
+public class ReverseEngine extends Engine {
+    private String winner = "Draw";
+
+    protected boolean getWinner(String turnPrompt, int[][] board, int currentPlayer, Scanner scanner) {
+        return true;
+
+    }
+
+     public void printRowSeparator(int size) {
+        System.out.print("-");
+        for (int i = 0; i < size; i++) {
+            System.out.print("-----");
+        }
+        System.out.println();
+    }
+
+    public void printRow(int[] row) {
+        int size = row.length;
+        for (int i = 0; i < size; i++) {
+            String mark = (row[i] == 1) ? "X" : ((row[i] == 2) ? "O" : " ");
+            System.out.print("| " + mark + " ");
+        }
+        System.out.println("|");
+    }
 
     @Override
-    protected boolean getWinner(String turnPrompt, int[][] board, int currentPlayer, Scanner scanner) {
-        System.out.println(turnPrompt);
-
-        int row = 0, col = 0;
-        while (true) {
-            if (currentPlayer == 1) {
-                row = getValidInt(scanner, "\nEnter row (0-2): ", 0, 2);
-                col = getValidInt(scanner, "Enter col (0-2): ", 0, 2);
-            } else {
-                while (true) {
-                    row = random.nextInt(3);
-                    col = random.nextInt(3);
-
-                    if (board[row][col] == 0) {
-                        break;
-                    }
-                }
-                System.out.printf("\nComputer chooses [%d,%d]\n", row, col);
-            }
-
-            if (board[row][col] != 0) {
-                System.out.printf("[%d,%d] is already filled! Please choose an empty cell.\n", row, col);
-            } else {
-                board[row][col] = currentPlayer;
-                boolean isWinner = checkHit(board, row, col);
-                
-                if (isWinner) {
-                    if (currentPlayer == 1) {
-                        System.out.println("Computer wins!");
-                        winner = "PLayer";
-                    } else if (currentPlayer ==2) {
-                        System.out.println("Player 1 wins!");
-                        winner = "Computer";
-
-                }
-                    return true;
-                }
-                return false;
-            }
-               
-            }
-
-           
+    protected int getBoardSize() {
+        return boardSize;
     }
 
-    private boolean checkHit(int[][] board, int row, int col) {
+    protected boolean checkHit(int[][] board, int row, int col) {
         int player = board[row][col];
-    
-        // Check rows
-        int rowCount = 0;
-        for (int i = 0; i < 3; i++) {
-            if (board[row][i] == player) {
-                rowCount++;
+
+        // Check row
+        int count = 0;
+        for (int c = 0; c < boardSize; c++) {
+            if (board[row][c] == player) {
+                count++;
+            } else {
+                count = 0;
             }
-        }
-        if (rowCount == 3) {
-            return true;
-        }
-    
-        // Check columns
-        int colCount = 0;
-        for (int i = 0; i < 3; i++) {
-            if (board[i][col] == player) {
-                colCount++;
-            }
-        }
-        if (colCount == 3) {
-            return true;
-        }
-    
-        // Check diagonals
-        if (row == col) {
-            int diagonalCount = 0;
-            for (int i = 0; i < 3; i++) {
-                if (board[i][i] == player) {
-                    diagonalCount++;
-                }
-            }
-            if (diagonalCount == 3) {
+
+            if (count == 3) {
                 return true;
             }
         }
-    
-        if (row + col == 2) {
-            int diagonalCount = 0;
-            for (int i = 0; i < 3; i++) {
-                if (board[i][2 - i] == player) {
-                    diagonalCount++;
-                }
+
+        // Check column
+        count = 0;
+        for (int r = 0; r < boardSize; r++) {
+            if (board[r][col] == player) {
+                count++;
+            } else {
+                count = 0;
             }
-            if (diagonalCount == 3) {
+
+            if (count == 3) {
                 return true;
             }
         }
-    
+
+        // Check diagonal (top-left to bottom-right)
+        count = 0;
+        int startRow = Math.max(row - col, 0);
+        int startCol = Math.max(col - row, 0);
+        for (int i = 0; i < Math.min(boardSize - startRow, boardSize - startCol); i++) {
+            if (board[startRow + i][startCol + i] == player) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if (count == 3) {
+                return true;
+            }
+        }
+
+        // Check diagonal (top-right to bottom-left)
+        count = 0;
+        startRow = Math.max(row - (boardSize - 1 - col), 0);
+        startCol = Math.min(col + row, boardSize - 1);
+        for (int i = 0; i < Math.min(boardSize - startRow, startCol + 1); i++) {
+            if (board[startRow + i][startCol - i] == player) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if (count == 3) {
+                return true;
+            }
+        }
+
         return false;
     }
-
+    
     public String getWinner() {
         return winner;
     }
     
+
 }
