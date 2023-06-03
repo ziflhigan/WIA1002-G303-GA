2eee0928ec93019f7b4ddaba1dc540740fcd8c1f
Calvin993 <calvinjmy993@gmail.com>
2023-06-03 21:41:04 +0800

    :fire: remove unused import

--- TTT/TicTacToeRegular.java
+++ TTT/TicTacToeRegular.java
@@ -9,7 +9,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.InputMismatchException;
-import java.util.Random;
 import java.util.Scanner;
 import java.util.Stack;
 
